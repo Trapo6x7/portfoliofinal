605ef284071aeed5bb3075c7d4d2f5d8aa1c8186
Trapo6x7 <matoufdanslezoo@gmail.com>
2025-01-26 20:22:03 +0100

    BUG :CSS JS

--- script.js
+++ script.js
@@ -1,56 +1,38 @@
 const div1 = document.querySelector("#toggleDiv1");
 div1.addEventListener("click", toggleDiv1);
-const div2 = document.querySelector("#toggleDiv2");
-div2.addEventListener("click", toggleDiv2);
 const div3 = document.querySelector("#toggleDiv3");
 div3.addEventListener("click", toggleDiv3);
 const div4 = document.querySelector("#toggleDiv4");
 div4.addEventListener("click", toggleDiv4);
 
-const divHide1 = document.querySelector(".none1");
-const divHide2 = document.querySelector(".none2");
+const divHide1 = document.querySelectorAll(".none1");
 const divHide3 = document.querySelectorAll(".none3");
 const divHide4 = document.querySelectorAll(".none4");
 const open1 = document.querySelector("#open1");
-const bw = document.querySelector("#bigwindow");
 const contener = document.querySelector("#basewindow");
 const center1 = document.querySelector("#center1");
+const windowContent = document.querySelector('.window2__content');
 
 function toggleDiv1() {
-  divHide1.classList.toggle("block");
-  divHide1.classList.toggle("relative");
+  divHide1.forEach(element => {
+    element.classList.toggle("block");
+    element.classList.toggle("relative");
+  });
   div1.classList.toggle("absolute");
-  div2.classList.toggle("hide");
   div3.classList.toggle("hide");
   div4.classList.toggle("hide");
-  bw.classList.toggle("flex");
   center1.classList.toggle("justify-center");
-  contener.classList.toggle("heightWindow");
-}
-
-
-function toggleDiv2() {
-  divHide2.classList.toggle("block");
-  divHide2.classList.toggle("relative2");
-  div2.classList.toggle("absolute2");
-  div1.classList.toggle("hide");
-  div3.classList.toggle("hide");
-  div4.classList.toggle("hide");
-  bw.classList.toggle("flex");
-  center1.classList.toggle("justify-center");
-  contener.classList.toggle("heightWindow2");
+  contener.classList.toggle("heightWindow3");
 }
 
 function toggleDiv3() {
   divHide3.forEach(element => {
     element.classList.toggle("block");
-  element.classList.toggle("relative");
+    element.classList.toggle("relative");
   });
   div3.classList.toggle("absolute");
-  div2.classList.toggle("hide");
   div1.classList.toggle("hide");
   div4.classList.toggle("hide");
-  // bw.classList.toggle("flex");
   center1.classList.toggle("justify-center");
   contener.classList.toggle("heightWindow3");
 }
@@ -58,13 +40,11 @@ function toggleDiv3() {
 function toggleDiv4() {
   divHide4.forEach(element => {
     element.classList.toggle("block");
-  element.classList.toggle("relative");
+    element.classList.toggle("relative");
   });
   div4.classList.toggle("absolute");
   div1.classList.toggle("hide");
   div3.classList.toggle("hide");
-  div2.classList.toggle("hide");
-  // bw.classList.toggle("flex");
   center1.classList.toggle("justify-center");
   contener.classList.toggle("heightWindow3");
 }
@@ -75,6 +55,3 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-
-
-
